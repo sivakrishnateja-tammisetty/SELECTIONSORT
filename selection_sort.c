e6da5b55196f32e06d73c8cfcdb14c5c9e9ef81b
Sivakrishnateja Tammisetty <98377305+sivakrishnateja-tammisetty@users.noreply.github.com>
2023-12-11 12:43:46 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -1,3 +1,65 @@
+* SELECTION SORT: select minimus in an array and place front
+steps:
+	1.select minimums and swap(Select minimum in an entire array and place at front).
+	2.select minimum in an unsorted array and swap.
+	3. repeat step 1 &2
+	4.  repeat step 1 &2
+	5.   repeat step 1 &2
+
+
+	Eg:
+	#include <bits/stdc++.h>
+using namespace std;
+void selection_sort(int *a,int n)
+{
+    for(int i=0;i<=n-2;i++)
+    {
+        int min=i;
+        for(int j=i;j<=n-1;j++)
+        {
+            if(a[j]<a[min])min=j;
+        }
+        int temp=a[min];
+        a[min]=a[i];
+        a[i]=temp;
+    }
+}
+void display(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+ int a[]={11,2,4,2,23,20};
+ int n=sizeof(a)/sizeof(a[0]);
+ selection_sort(a,n);
+ display(a, n);
+ 
+    return 0;
+}
+o/p:
+	2 2 4 11 20 23 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
 #include<stdio.h>
 void swap(int *x,int *y)
 {
